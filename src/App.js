61b6543d77b60aefda227d682f8777a7fa4b3d9b
vinay_jadaun <vinay11112003@gmail.com>
2024-04-16 22:15:08 +0530

    version v4.0

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import {Routes,Route} from 'react-router-dom';
 import Homepage from "./pages/home";
 import Roompage from "./pages/room";
+// import dotenv from "dotenv";
+// dotenv.config()
 function App() {
   return (
     <Routes>
--- src/pages/room/index.jsx
+++ src/pages/room/index.jsx
@@ -1,20 +1,29 @@
-import React from 'react'
+import React, {useState,useEffect} from 'react'
 import {useParams} from "react-router-dom";
 import {ZegoUIKitPrebuilt} from "@zegocloud/zego-uikit-prebuilt"
+// import dotenv from "dotenv";
+// dotenv.config()
+
 import "./s.css";
 function Roompage() {
+  const [Url,seturl]=useState('none');
+  useEffect(()=>{
+    seturl(window.location.href)
+  },[])
     const{roomId}=useParams();
+    const vin=window.location.href
     const myMeeting = async(element)=>{
         const appID=40142866;
         const serverSecret="a7eb699d70e299fb71b040c29f02948b";
         const kitToken=ZegoUIKitPrebuilt.generateKitTokenForTest(appID,serverSecret,roomId,Date.now().toString(),"Vinay Jadaun");
         const zc=ZegoUIKitPrebuilt.create(kitToken);
+        // ${roomId}
         zc.joinRoom({
           container:element,
           sharedLinks:[
             {
               name:'copy link',
-              url:`https://vercel.com/vinay-jadauns-projects/group-meeting-app.vercel.app/${roomId}`,
+              url:vin,
             }
           ],
           scenario:{
